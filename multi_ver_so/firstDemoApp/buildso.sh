e3088ab53ba74e1e18ecd3a7fc4b718316b6bbc3
woyaozuomanong <peikingren@sohu.com>
2019-03-01 08:06:17 -0500

    this amendment fix the bug.

--- multi_ver_so/firstDemoApp/buildso.sh
+++ multi_ver_so/firstDemoApp/buildso.sh
@@ -1,4 +1,5 @@
 gcc -Wall -g -O0 -fPIC -c simple.c
 gcc -shared simple.o -Wl,--version-script,simpleVersionScript -o libsimple.so.1.0.0
 rm ../sharedLib/libsimple.so
-ln -s libsimple.so.1.0.0 ../sharedLib/libsimple.so
+mv libsimple.so.1.0.0 ../sharedLib
+ln -s ../sharedLib/libsimple.so.1.0.0 ../sharedLib/libsimple.so
